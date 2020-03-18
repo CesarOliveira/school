f2a73fc4eb70b54180d8f6ae1d5e9b12c4d3e949
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-03-18 19:33:46 -0300

    Finish tests on Course repository

--- api/spec/unit/repositories/course_repository_spec.rb
+++ api/spec/unit/repositories/course_repository_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe CourseRepository, type: :repository do
   end
 
   let(:result_query) { double }
+  let(:course) { double }
 
   describe '.find_all' do
     before do
@@ -43,10 +44,58 @@ RSpec.describe CourseRepository, type: :repository do
       described_class.find_by_name(data[:name])
     end
 
-    it 'should call the where method on the Course model and return true' do
+    it 'should call the where method on the Course model with the rigth query' do
       expect(Course)
         .to have_received(:where).with('lower(name) LIKE ?', "%#{data[:name].downcase}%")
     end
   end
 
+  describe '.create' do
+    before do
+      allow(Course).to receive(:new).with(data)
+
+      described_class.create(data)
+    end
+
+    it 'should call the new method on the Course model' do
+      expect(Course).to have_received(:new).with(data)
+    end
+  end
+
+  describe '.save' do
+    before do
+      allow(course).to receive(:save)
+
+      described_class.save(course)
+    end
+
+    it 'should call the new save on the course instance' do
+      expect(course).to have_received(:save)
+    end
+  end
+
+  describe '.update' do
+    before do
+      allow(course).to receive(:update).with(data)
+
+      described_class.update(course, data)
+    end
+
+    it 'should call the update method on the course instance' do
+      expect(course).to have_received(:update).with(data)
+    end
+  end
+
+
+  describe '.destroy' do
+    before do
+      allow(course).to receive(:destroy)
+
+      described_class.destroy(course)
+    end
+
+    it 'should call the new destroy on the course instance' do
+      expect(course).to have_received(:destroy)
+    end
+  end
 end
